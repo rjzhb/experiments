7b6725ab38b5ccfe82022524ee5c983194f5c046
rjzhb <986215159@qq.com>
2024-03-03 17:03:39 +0800

    update

--- vectordb/src/common/bustub_instance.cpp
+++ vectordb/src/common/bustub_instance.cpp
@@ -42,7 +42,7 @@ namespace bustub {
 
 auto BustubInstance::MakeExecutorContext(Transaction *txn, bool is_modify) -> std::unique_ptr<ExecutorContext> {
   return std::make_unique<ExecutorContext>(txn, catalog_.get(), buffer_pool_manager_.get(), txn_manager_.get(),
-                                           lock_manager_.get(), is_modify);
+										   lock_manager_.get(), is_modify);
 }
 
 BustubInstance::BustubInstance(const std::string &db_file_name, size_t bpm_size) {
@@ -59,11 +59,11 @@ BustubInstance::BustubInstance(const std::string &db_file_name, size_t bpm_size)
   // We need more frames for GenerateTestTable to work. Therefore, we use 128 instead of the default
   // buffer pool size specified in `config.h`.
   try {
-    buffer_pool_manager_ =
-        std::make_unique<BufferPoolManager>(bpm_size, disk_manager_.get(), LRUK_REPLACER_K, log_manager_.get());
+	buffer_pool_manager_ =
+		std::make_unique<BufferPoolManager>(bpm_size, disk_manager_.get(), LRUK_REPLACER_K, log_manager_.get());
   } catch (NotImplementedException &e) {
-    std::cerr << "BufferPoolManager is not implemented, only mock tables are supported." << std::endl;
-    buffer_pool_manager_ = nullptr;
+	std::cerr << "BufferPoolManager is not implemented, only mock tables are supported." << std::endl;
+	buffer_pool_manager_ = nullptr;
   }
 
 // Transaction (txn) related.
@@ -86,7 +86,7 @@ BustubInstance::BustubInstance(const std::string &db_file_name, size_t bpm_size)
 #ifndef DISABLE_CHECKPOINT_MANAGER
   // Checkpoint related.
   checkpoint_manager_ =
-      std::make_unique<CheckpointManager>(txn_manager_.get(), log_manager_.get(), buffer_pool_manager_.get());
+	  std::make_unique<CheckpointManager>(txn_manager_.get(), log_manager_.get(), buffer_pool_manager_.get());
 #endif
 
   // Catalog related.
@@ -112,11 +112,11 @@ BustubInstance::BustubInstance(size_t bpm_size) {
   // We need more frames for GenerateTestTable to work. Therefore, we use 128 instead of the default
   // buffer pool size specified in `config.h`.
   try {
-    buffer_pool_manager_ =
-        std::make_unique<BufferPoolManager>(bpm_size, disk_manager_.get(), LRUK_REPLACER_K, log_manager_.get());
+	buffer_pool_manager_ =
+		std::make_unique<BufferPoolManager>(bpm_size, disk_manager_.get(), LRUK_REPLACER_K, log_manager_.get());
   } catch (NotImplementedException &e) {
-    std::cerr << "BufferPoolManager is not implemented, only mock tables are supported." << std::endl;
-    buffer_pool_manager_ = nullptr;
+	std::cerr << "BufferPoolManager is not implemented, only mock tables are supported." << std::endl;
+	buffer_pool_manager_ = nullptr;
   }
 
 #ifndef DISABLE_LOCK_MANAGER
@@ -137,7 +137,7 @@ BustubInstance::BustubInstance(size_t bpm_size) {
 #ifndef DISABLE_CHECKPOINT_MANAGER
   // Checkpoint related.
   checkpoint_manager_ =
-      std::make_unique<CheckpointManager>(txn_manager_.get(), log_manager_.get(), buffer_pool_manager_.get());
+	  std::make_unique<CheckpointManager>(txn_manager_.get(), log_manager_.get(), buffer_pool_manager_.get());
 #endif
 
   // Catalog related.
@@ -151,16 +151,16 @@ BustubInstance::BustubInstance(size_t bpm_size) {
 
 void BustubInstance::CmdDbgMvcc(const std::vector<std::string> &params, ResultWriter &writer) {
   if (params.size() != 2) {
-    writer.OneCell("please provide a table name");
-    return;
+	writer.OneCell("please provide a table name");
+	return;
   }
   const auto &table = params[1];
   writer.OneCell("please view the result in the BusTub console (or Chrome DevTools console), table=" + table);
   std::shared_lock<std::shared_mutex> lck(catalog_lock_);
   auto table_info = catalog_->GetTable(table);
   if (table_info == nullptr) {
-    writer.OneCell("table " + table + " not found");
-    return;
+	writer.OneCell("table " + table + " not found");
+	return;
   }
   TxnMgrDbg("\\dbgmvcc", txn_manager_.get(), table_info, table_info->table_.get());
 }
@@ -174,12 +174,12 @@ void BustubInstance::CmdDisplayTables(ResultWriter &writer) {
   writer.WriteHeaderCell("cols");
   writer.EndHeader();
   for (const auto &name : table_names) {
-    writer.BeginRow();
-    const auto *table_info = catalog_->GetTable(name);
-    writer.WriteCell(fmt::format("{}", table_info->oid_));
-    writer.WriteCell(table_info->name_);
-    writer.WriteCell(table_info->schema_.ToString());
-    writer.EndRow();
+	writer.BeginRow();
+	const auto *table_info = catalog_->GetTable(name);
+	writer.WriteCell(fmt::format("{}", table_info->oid_));
+	writer.WriteCell(table_info->name_);
+	writer.WriteCell(table_info->schema_.ToString());
+	writer.EndRow();
   }
   writer.EndTable();
 }
@@ -194,14 +194,14 @@ void BustubInstance::CmdDisplayIndices(ResultWriter &writer) {
   writer.WriteHeaderCell("index_cols");
   writer.EndHeader();
   for (const auto &table_name : table_names) {
-    for (const auto *index_info : catalog_->GetTableIndexes(table_name)) {
-      writer.BeginRow();
-      writer.WriteCell(table_name);
-      writer.WriteCell(fmt::format("{}", index_info->index_oid_));
-      writer.WriteCell(index_info->name_);
-      writer.WriteCell(index_info->key_schema_.ToString());
-      writer.EndRow();
-    }
+	for (const auto *index_info : catalog_->GetTableIndexes(table_name)) {
+	  writer.BeginRow();
+	  writer.WriteCell(table_name);
+	  writer.WriteCell(fmt::format("{}", index_info->index_oid_));
+	  writer.WriteCell(index_info->name_);
+	  writer.WriteCell(index_info->key_schema_.ToString());
+	  writer.EndRow();
+	}
   }
   writer.EndTable();
 }
@@ -231,149 +231,171 @@ see the execution plan of your query.
   WriteOneCell(help, writer);
 }
 
-auto BustubInstance::ExecuteSql(const std::string &sql, ResultWriter &writer,
-                                std::shared_ptr<CheckOptions> check_options) -> bool {
+// 在数据库实例上执行给定的SQL语句。
+// 这个方法负责管理事务的生命周期（如果需要），并处理SQL执行过程中可能出现的异常。
+auto BustubInstance::ExecuteSql(const std::string &sql,
+								ResultWriter &writer,
+								std::shared_ptr<CheckOptions> check_options) -> bool {
+  // 检查当前是否有活动的事务。
   bool is_local_txn = current_txn_ != nullptr;
+  // 如果有活动的事务，则使用它；否则，从事务管理器开始一个新的事务。
   auto *txn = is_local_txn ? current_txn_ : txn_manager_->Begin();
+
   try {
-    auto result = ExecuteSqlTxn(sql, writer, txn, std::move(check_options));
-    if (!is_local_txn) {
-      auto res = txn_manager_->Commit(txn);
-      if (!res) {
-        throw Exception("failed to commit txn");
-      }
-    }
-    return result;
+	// 执行SQL语句。如果成功，根据事务的类型（本地或全局）来提交事务。
+	auto result = ExecuteSqlTxn(sql, writer, txn, std::move(check_options));
+	// 如果这不是一个本地事务（即，这个事务是为这次SQL执行新创建的），则尝试提交事务。
+	if (!is_local_txn) {
+	  auto res = txn_manager_->Commit(txn);
+	  // 如果提交失败，抛出异常。
+	  if (!res) {
+		throw Exception("failed to commit txn");
+	  }
+	}
+	// 返回SQL执行的结果。
+	return result;
   } catch (bustub::Exception &ex) {
-    txn_manager_->Abort(txn);
-    current_txn_ = nullptr;
-    throw ex;
+	// 如果在SQL执行过程中发生异常，中止事务，并清理当前事务指针，然后重新抛出异常。
+	txn_manager_->Abort(txn);
+	current_txn_ = nullptr;
+	throw ex;
   }
 }
 
-auto BustubInstance::ExecuteSqlTxn(const std::string &sql, ResultWriter &writer, Transaction *txn,
-                                   std::shared_ptr<CheckOptions> check_options) -> bool {
+// 在特定事务环境下执行SQL语句，处理内部命令和标准SQL查询。
+auto BustubInstance::ExecuteSqlTxn(const std::string &sql,
+								   ResultWriter &writer,
+								   Transaction *txn,
+								   std::shared_ptr<CheckOptions> check_options) -> bool {
+  // 检查是否是内部命令（以'\'开头的命令，类似于psql的内部命令）
   if (!sql.empty() && sql[0] == '\\') {
-    // Internal meta-commands, like in `psql`.
-    if (sql == "\\dt") {
-      CmdDisplayTables(writer);
-      return true;
-    }
-    if (sql == "\\di") {
-      CmdDisplayIndices(writer);
-      return true;
-    }
-    if (sql == "\\help") {
-      CmdDisplayHelp(writer);
-      return true;
-    }
-    if (StringUtil::StartsWith(sql, "\\dbgmvcc")) {
-      auto split = StringUtil::Split(sql, " ");
-      CmdDbgMvcc(split, writer);
-      return true;
-    }
-    if (StringUtil::StartsWith(sql, "\\txn")) {
-      auto split = StringUtil::Split(sql, " ");
-      CmdTxn(split, writer);
-      return true;
-    }
-    throw Exception(fmt::format("unsupported internal command: {}", sql));
+	// 处理不同的内部命令
+	if (sql == "\\dt") {
+	  CmdDisplayTables(writer); // 显示数据库中的所有表
+	  return true;
+	}
+	if (sql == "\\di") {
+	  CmdDisplayIndices(writer); // 显示数据库中的所有索引
+	  return true;
+	}
+	if (sql == "\\help") {
+	  CmdDisplayHelp(writer); // 显示帮助信息
+	  return true;
+	}
+	if (StringUtil::StartsWith(sql, "\\dbgmvcc")) {
+	  auto split = StringUtil::Split(sql, " ");
+	  CmdDbgMvcc(split, writer); // 调试MVCC信息
+	  return true;
+	}
+	if (StringUtil::StartsWith(sql, "\\txn")) {
+	  auto split = StringUtil::Split(sql, " ");
+	  CmdTxn(split, writer); // 处理事务相关的命令
+	  return true;
+	}
+	// 抛出异常，未知的内部命令
+	throw Exception(fmt::format("unsupported internal command: {}", sql));
   }
 
-  bool is_successful = true;
+  bool is_successful = true; // 记录执行是否成功
 
+  // 获取对目录的共享锁
   std::shared_lock<std::shared_mutex> l(catalog_lock_);
+  // 绑定和解析SQL语句
   bustub::Binder binder(*catalog_);
   binder.ParseAndSave(sql);
-  l.unlock();
+  l.unlock(); // 解锁目录
 
+  // 遍历所有解析后的语句节点
   for (auto *stmt : binder.statement_nodes_) {
-    auto statement = binder.BindStatement(stmt);
+	auto statement = binder.BindStatement(stmt); // 绑定语句
 
-    bool is_delete = false;
+	bool is_delete = false;
 
-    switch (statement->type_) {
-      case StatementType::CREATE_STATEMENT: {
-        const auto &create_stmt = dynamic_cast<const CreateStatement &>(*statement);
-        HandleCreateStatement(txn, create_stmt, writer);
-        continue;
-      }
-      case StatementType::INDEX_STATEMENT: {
-        const auto &index_stmt = dynamic_cast<const IndexStatement &>(*statement);
-        HandleIndexStatement(txn, index_stmt, writer);
-        continue;
-      }
-      case StatementType::VARIABLE_SHOW_STATEMENT: {
-        const auto &show_stmt = dynamic_cast<const VariableShowStatement &>(*statement);
-        HandleVariableShowStatement(txn, show_stmt, writer);
-        continue;
-      }
-      case StatementType::VARIABLE_SET_STATEMENT: {
-        const auto &set_stmt = dynamic_cast<const VariableSetStatement &>(*statement);
-        HandleVariableSetStatement(txn, set_stmt, writer);
-        continue;
-      }
-      case StatementType::EXPLAIN_STATEMENT: {
-        const auto &explain_stmt = dynamic_cast<const ExplainStatement &>(*statement);
-        HandleExplainStatement(txn, explain_stmt, writer);
-        continue;
-      }
-      case StatementType::TRANSACTION_STATEMENT: {
-        const auto &txn_stmt = dynamic_cast<const TransactionStatement &>(*statement);
-        HandleTxnStatement(txn, txn_stmt, writer);
-        continue;
-      }
-      case StatementType::DELETE_STATEMENT:
-      case StatementType::UPDATE_STATEMENT:
-        is_delete = true;
-      default:
-        break;
-    }
+	// 根据语句类型进行不同的处理
+	switch (statement->type_) {
+	  case StatementType::CREATE_STATEMENT: {
+		const auto &create_stmt = dynamic_cast<const CreateStatement &>(*statement);
+		HandleCreateStatement(txn, create_stmt, writer); // 处理创建表语句
+		continue;
+	  }
+	  case StatementType::INDEX_STATEMENT: {
+		const auto &index_stmt = dynamic_cast<const IndexStatement &>(*statement);
+		HandleIndexStatement(txn, index_stmt, writer); // 处理创建索引语句
+		continue;
+	  }
+	  case StatementType::VARIABLE_SHOW_STATEMENT: {
+		const auto &show_stmt = dynamic_cast<const VariableShowStatement &>(*statement);
+		HandleVariableShowStatement(txn, show_stmt, writer); // 处理显示变量语句
+		continue;
+	  }
+	  case StatementType::VARIABLE_SET_STATEMENT: {
+		const auto &set_stmt = dynamic_cast<const VariableSetStatement &>(*statement);
+		HandleVariableSetStatement(txn, set_stmt, writer); // 处理设置变量语句
+		continue;
+	  }
+	  case StatementType::EXPLAIN_STATEMENT: {
+		const auto &explain_stmt = dynamic_cast<const ExplainStatement &>(*statement);
+		HandleExplainStatement(txn, explain_stmt, writer); // 处理解释计划语句
+		continue;
+	  }
+	  case StatementType::TRANSACTION_STATEMENT: {
+		const auto &txn_stmt = dynamic_cast<const TransactionStatement &>(*statement);
+		HandleTxnStatement(txn, txn_stmt, writer); // 处理事务语句
+		continue;
+	  }
+	  case StatementType::DELETE_STATEMENT:
+	  case StatementType::UPDATE_STATEMENT:
+		is_delete = true; // 标记为删除或更新语句
+	  default:
+		break;
+	}
 
-    std::shared_lock<std::shared_mutex> l(catalog_lock_);
+	// 再次获取目录的共享锁
+	std::shared_lock<std::shared_mutex> l(catalog_lock_);
 
-    // Plan the query.
-    bustub::Planner planner(*catalog_);
-    planner.PlanQuery(*statement);
+	// 规划查询
+	bustub::Planner planner(*catalog_);
+	planner.PlanQuery(*statement);
 
-    // Optimize the query.
-    bustub::Optimizer optimizer(*catalog_, session_variables_);
-    auto optimized_plan = optimizer.Optimize(planner.plan_);
+	// 优化查询
+	bustub::Optimizer optimizer(*catalog_, session_variables_);
+	auto optimized_plan = optimizer.Optimize(planner.plan_);
 
-    l.unlock();
+	l.unlock(); // 解锁目录
 
-    // Execute the query.
-    auto exec_ctx = MakeExecutorContext(txn, is_delete);
-    if (check_options != nullptr) {
-      exec_ctx->InitCheckOptions(std::move(check_options));
-    }
-    std::vector<Tuple> result_set{};
-    is_successful &= execution_engine_->Execute(optimized_plan, &result_set, txn, exec_ctx.get());
+	// 执行查询
+	auto exec_ctx = MakeExecutorContext(txn, is_delete);
+	if (check_options != nullptr) {
+	  exec_ctx->InitCheckOptions(std::move(check_options)); // 初始化检查选项
+	}
+	std::vector<Tuple> result_set{}; // 结果集
+	is_successful &= execution_engine_->Execute(optimized_plan, &result_set, txn, exec_ctx.get()); // 执行优化后的计划
 
-    // Return the result set as a vector of string.
-    auto schema = planner.plan_->OutputSchema();
+	// 将结果集转换为字符串并写入writer
+	auto schema = planner.plan_->OutputSchema(); // 获取输出模式
 
-    // Generate header for the result set.
-    writer.BeginTable(false);
-    writer.BeginHeader();
-    for (const auto &column : schema.GetColumns()) {
-      writer.WriteHeaderCell(column.GetName());
-    }
-    writer.EndHeader();
+	// 生成结果集的表头
+	writer.BeginTable(false);
+	writer.BeginHeader();
+	for (const auto &column : schema.GetColumns()) {
+	  writer.WriteHeaderCell(column.GetName()); // 写入列名
+	}
+	writer.EndHeader();
 
-    // Transforming result set into strings.
-    for (const auto &tuple : result_set) {
-      writer.BeginRow();
-      for (uint32_t i = 0; i < schema.GetColumnCount(); i++) {
-        writer.WriteCell(tuple.GetValue(&schema, i).ToString());
-      }
-      writer.EndRow();
-    }
-    writer.EndTable();
+	// 将结果集的每一行转换为字符串
+	for (const auto &tuple : result_set) {
+	  writer.BeginRow();
+	  for (uint32_t i = 0; i < schema.GetColumnCount(); i++) {
+		writer.WriteCell(tuple.GetValue(&schema, i).ToString()); // 写入每个单元格的值
+	  }
+	  writer.EndRow();
+	}
+	writer.EndTable();
   }
 
-  return is_successful;
+  return is_successful; // 返回执行结果
 }
+
 
 /**
  * FOR TEST ONLY. Generate test tables in this BusTub instance.
@@ -403,7 +425,7 @@ void BustubInstance::GenerateMockTable() {
 
   std::shared_lock<std::shared_mutex> l(catalog_lock_);
   for (auto table_name = &mock_table_list[0]; *table_name != nullptr; table_name++) {
-    catalog_->CreateTable(txn, *table_name, GetMockTableSchemaOf(*table_name), false);
+	catalog_->CreateTable(txn, *table_name, GetMockTableSchemaOf(*table_name), false);
   }
   l.unlock();
 
@@ -412,7 +434,7 @@ void BustubInstance::GenerateMockTable() {
 
 BustubInstance::~BustubInstance() {
   if (enable_logging) {
-    log_manager_->StopFlushThread();
+	log_manager_->StopFlushThread();
   }
 }
 
@@ -422,49 +444,50 @@ void BustubInstance::EnableManagedTxn() { managed_txn_mode_ = true; }
 /** Get the current transaction. */
 auto BustubInstance::CurrentManagedTxn() -> Transaction * { return current_txn_; }
 
-void BustubInstance::CmdTxn(const std::vector<std::string> &params, ResultWriter &writer) {
+void BustubInstance::CmdTxn(const std::vector<std::string> &params,
+							ResultWriter &writer) {
   if (!managed_txn_mode_) {
-    writer.OneCell("only supported in managed mode, please use bustub-shell");
-    return;
+	writer.OneCell("only supported in managed mode, please use bustub-shell");
+	return;
   }
   auto dump_current_txn = [&](const std::string &prefix) {
-    writer.OneCell(fmt::format("{}txn_id={} txn_real_id={} read_ts={} commit_ts={} status={} iso_lvl={}", prefix,
-                               current_txn_->GetTransactionIdHumanReadable(), current_txn_->GetTransactionId(),
-                               current_txn_->GetReadTs(), current_txn_->GetCommitTs(),
-                               current_txn_->GetTransactionState(), current_txn_->GetIsolationLevel()));
+	writer.OneCell(fmt::format("{}txn_id={} txn_real_id={} read_ts={} commit_ts={} status={} iso_lvl={}", prefix,
+							   current_txn_->GetTransactionIdHumanReadable(), current_txn_->GetTransactionId(),
+							   current_txn_->GetReadTs(), current_txn_->GetCommitTs(),
+							   current_txn_->GetTransactionState(), current_txn_->GetIsolationLevel()));
   };
   if (params.size() == 1) {
-    if (current_txn_ != nullptr) {
-      dump_current_txn("");
-    } else {
-      writer.OneCell("no active txn, each statement starts a new txn.");
-    }
-    return;
+	if (current_txn_ != nullptr) {
+	  dump_current_txn("");
+	} else {
+	  writer.OneCell("no active txn, each statement starts a new txn.");
+	}
+	return;
   }
   if (params.size() == 2) {
-    const std::string &param1 = params[1];
-    if (param1 == "gc") {
-      txn_manager_->GarbageCollection();
-      writer.OneCell("GC complete");
-      return;
-    }
-    auto txn_id = std::stoi(param1);
-    if (txn_id == -1) {
-      dump_current_txn("pause current txn ");
-      current_txn_ = nullptr;
-      return;
-    }
-    auto iter = txn_manager_->txn_map_.find(txn_id);
-    if (iter == txn_manager_->txn_map_.end()) {
-      iter = txn_manager_->txn_map_.find(txn_id + TXN_START_ID);
-      if (iter == txn_manager_->txn_map_.end()) {
-        writer.OneCell("cannot find txn.");
-        return;
-      }
-    }
-    current_txn_ = iter->second.get();
-    dump_current_txn("switch to new txn ");
-    return;
+	const std::string &param1 = params[1];
+	if (param1 == "gc") {
+	  txn_manager_->GarbageCollection();
+	  writer.OneCell("GC complete");
+	  return;
+	}
+	auto txn_id = std::stoi(param1);
+	if (txn_id == -1) {
+	  dump_current_txn("pause current txn ");
+	  current_txn_ = nullptr;
+	  return;
+	}
+	auto iter = txn_manager_->txn_map_.find(txn_id);
+	if (iter == txn_manager_->txn_map_.end()) {
+	  iter = txn_manager_->txn_map_.find(txn_id + TXN_START_ID);
+	  if (iter == txn_manager_->txn_map_.end()) {
+		writer.OneCell("cannot find txn.");
+		return;
+	  }
+	}
+	current_txn_ = iter->second.get();
+	dump_current_txn("switch to new txn ");
+	return;
   }
   writer.OneCell("unsupported txn cmd.");
 }
--- vectordb/tools/shell/shell.cpp
+++ vectordb/tools/shell/shell.cpp
@@ -10,113 +10,125 @@
 #include "linenoise/linenoise.h"
 #include "utf8proc/utf8proc.h"
 
+#include "utf8proc/utf8proc.h"
+
+// 计算UTF-8字符串的显示宽度
 auto GetWidthOfUtf8(const void *beg, const void *end, size_t *width) -> int {
-  size_t computed_width = 0;
+  size_t computed_width = 0; // 计算得到的宽度
   utf8proc_ssize_t n;
+  // 计算字符串的大小
   utf8proc_ssize_t size = static_cast<const char *>(end) - static_cast<const char *>(beg);
-  auto pstring = static_cast<utf8proc_uint8_t const *>(beg);
-  utf8proc_int32_t data;
+  auto pstring = static_cast<utf8proc_uint8_t const *>(beg); // 指向字符串起始位置的指针
+  utf8proc_int32_t data; // 存储当前字符的Unicode码点
+  // 遍历字符串
   while ((n = utf8proc_iterate(pstring, size, &data)) > 0) {
-    computed_width += utf8proc_charwidth(data);
-    pstring += n;
-    size -= n;
+	computed_width += utf8proc_charwidth(data); // 计算并累加字符的显示宽度
+	pstring += n; // 移动指针，指向下一个字符
+	size -= n; // 减少剩余大小
   }
-  *width = computed_width;
+  *width = computed_width; // 返回计算得到的宽度
   return 0;
 }
 
-// NOLINTNEXTLINE
+// 主函数
 auto main(int argc, char **argv) -> int {
-  ft_set_u8strwid_func(&GetWidthOfUtf8);
+  ft_set_u8strwid_func(&GetWidthOfUtf8); // 设置计算字符串宽度的函数
 
-  auto bustub = std::make_unique<bustub::BustubInstance>("test.db");
+  auto bustub = std::make_unique<bustub::BustubInstance>("test.db"); // 创建数据库实例
 
-  auto default_prompt = "bustub> ";
-  auto emoji_prompt = "\U0001f6c1> ";  // the bathtub emoji
-  bool use_emoji_prompt = false;
-  bool disable_tty = false;
+  auto default_prompt = "bustub> "; // 默认提示符
+  auto emoji_prompt = "\U0001f6c1> ";  // 浴缸emoji作为提示符
+  bool use_emoji_prompt = false; // 是否使用emoji提示符
+  bool disable_tty = false; // 是否禁用TTY
 
+  // 处理命令行参数
   for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--emoji-prompt") == 0) {
-      use_emoji_prompt = true;
-      break;
-    }
-    if (strcmp(argv[i], "--disable-tty") == 0) {
-      disable_tty = true;
-      break;
-    }
+	if (strcmp(argv[i], "--emoji-prompt") == 0) {
+	  use_emoji_prompt = true;
+	  break;
+	}
+	if (strcmp(argv[i], "--disable-tty") == 0) {
+	  disable_tty = true;
+	  break;
+	}
   }
 
-  bustub->GenerateMockTable();
+  bustub->GenerateMockTable(); // 生成模拟表
 
   if (bustub->buffer_pool_manager_ != nullptr) {
-    bustub->GenerateTestTable();
+	bustub->GenerateTestTable(); // 生成测试表
   }
 
-  bustub->EnableManagedTxn();
+  bustub->EnableManagedTxn(); // 启用事务管理
 
   std::cout << "Welcome to the BusTub shell! Type \\help to learn more." << std::endl << std::endl;
 
-  linenoiseHistorySetMaxLen(1024);
-  linenoiseSetMultiLine(1);
+  linenoiseHistorySetMaxLen(1024); // 设置历史记录最大长度
+  linenoiseSetMultiLine(1); // 启用多行输入
 
-  auto prompt = use_emoji_prompt ? emoji_prompt : default_prompt;
+  auto prompt = use_emoji_prompt ? emoji_prompt : default_prompt; // 根据参数选择提示符
 
+  // 循环读取和处理用户输入
   while (true) {
-    std::string query;
-    bool first_line = true;
-    while (true) {
-      std::string context_prompt = prompt;
-      auto *txn = bustub->CurrentManagedTxn();
-      if (txn != nullptr) {
-        if (txn->GetTransactionState() != bustub::TransactionState::RUNNING) {
-          context_prompt =
-              fmt::format("txn{} ({})> ", txn->GetTransactionIdHumanReadable(), txn->GetTransactionState());
-        } else {
-          context_prompt = fmt::format("txn{}> ", txn->GetTransactionIdHumanReadable());
-        }
-      }
-      std::string line_prompt = first_line ? context_prompt : "... ";
-      if (!disable_tty) {
-        char *query_c_str = linenoise(line_prompt.c_str());
-        if (query_c_str == nullptr) {
-          return 0;
-        }
-        query += query_c_str;
-        linenoiseFree(query_c_str);
-        if (bustub::StringUtil::EndsWith(query, ";") || bustub::StringUtil::StartsWith(query, "\\")) {
-          break;
-        }
-        query += " ";
-      } else {
-        std::string query_line;
-        std::cout << line_prompt;
-        std::getline(std::cin, query_line);
-        if (!std::cin) {
-          return 0;
-        }
-        query += query_line;
-        if (bustub::StringUtil::EndsWith(query, ";") || bustub::StringUtil::StartsWith(query, "\\")) {
-          break;
-        }
-        query += "\n";
-      }
-      first_line = false;
-    }
+	std::string query;
+	bool first_line = true;
+	while (true) {
+	  std::string context_prompt = prompt; // 设置当前提示符
+	  auto *txn = bustub->CurrentManagedTxn(); // 获取当前事务
+	  if (txn != nullptr) {
+		// 根据事务状态设置提示符
+		if (txn->GetTransactionState() != bustub::TransactionState::RUNNING) {
+		  context_prompt =
+			  fmt::format("txn{} ({})> ", txn->GetTransactionIdHumanReadable(), txn->GetTransactionState());
+		} else {
+		  context_prompt = fmt::format("txn{}> ", txn->GetTransactionIdHumanReadable());
+		}
+	  }
+	  std::string line_prompt = first_line ? context_prompt : "... "; // 设置行提示符
+	  if (!disable_tty) {
+		char *query_c_str = linenoise(line_prompt.c_str()); // 读取用户输入
+		if (query_c_str == nullptr) {
+		  return 0; // 如果用户输入为空，则退出
+		}
+		query += query_c_str; // 添加到查询字符串
+		linenoiseFree(query_c_str); // 释放内存
+		// 检查是否结束输入
+		if (bustub::StringUtil::EndsWith(query, ";") || bustub::StringUtil::StartsWith(query, "\\")) {
+		  break;
+		}
+		query += " "; // 添加空格，准备下一行输入
+	  } else {
+		// 如果禁用了TTY，直接从标准输入读取
+		std::string query_line;
+		std::cout << line_prompt;
+		std::getline(std::cin, query_line);
+		if (!std::cin) {
+		  return 0; // 如果读取失败，则退出
+		}
+		query += query_line; // 添加到查询字符串
+		if (bustub::StringUtil::EndsWith(query, ";") || bustub::StringUtil::StartsWith(query, "\\")) {
+		  break;
+		}
+		query += "\n"; // 添加换行符，准备下一行输入
+	  }
+	  first_line = false; // 更新标志，表示已经处理过第一行
+	}
 
-    if (!disable_tty) {
-      linenoiseHistoryAdd(query.c_str());
-    }
+	if (!disable_tty) {
+	  linenoiseHistoryAdd(query.c_str()); // 添加到历史记录
+	}
 
-    try {
-      auto writer = bustub::FortTableWriter();
-      bustub->ExecuteSql(query, writer);
-      for (const auto &table : writer.tables_) {
-        std::cout << table << std::flush;
-      }
-    } catch (bustub::Exception &ex) {
-      std::cerr << ex.what() << std::endl;
-    }
+	// 尝试执行SQL查询
+	try {
+	  auto writer = bustub::FortTableWriter(); // 创建表格写入器
+	  bustub->ExecuteSql(query, writer); // 执行SQL查询
+	  // 打印查询结果
+	  for (const auto &table : writer.tables_) {
+		std::cout << table << std::flush;
+	  }
+	} catch (bustub::Exception &ex) {
+	  std::cerr << ex.what() << std::endl; // 打印异常信息
+	}
   }
 
   return 0;
